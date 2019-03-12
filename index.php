6f9360163e2c43562c0cea447b24ae6420d313f4
cetecom <cetecom@LCV10809>
2019-03-11 22:37:11 -0300

    ejemplo2 css

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <link href="estiloTest.css" rel="stylesheet" type="text/css"/>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width">
+        <title></title>
+    </head>
+    <body>
+        
+        
+        <div id="Menu"> Menu</div>
+        <div id="Head"> Esta es el Head</div>
+        <div id="Contenedor"> este es el contenedor</div>
+        
+    </body>
+</html>
